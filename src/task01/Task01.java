6ef031ad275e20f46b024bd16a2158c84ed58575
inspshvager <gabweglarz@gmail.com>
2025-06-02 22:20:48 +0200

    Second Commit

--- src/task01/Task01.java
+++ src/task01/Task01.java
@@ -2,7 +2,8 @@ package task01;
 
 public class Task01 {
     public static void main(String[] args) {
-        Swiatlo swiatlo = Swiatlo.CZERWONE;
-        System.out.println(swiatlo.getKomunikat());
+        System.out.println(Swiatlo.CZERWONE.getKomunikat());
+        System.out.println(Swiatlo.ZOLTE.getKomunikat());
+        System.out.println(Swiatlo.ZIELONE.getKomunikat());
     }
 }
